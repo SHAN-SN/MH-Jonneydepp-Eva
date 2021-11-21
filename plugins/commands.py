477fc521c02b079731f10346c8276ca8465471e3
SHAN-SN <85439835+SHAN-SN@users.noreply.github.com>
2021-11-21 13:39:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -78,7 +78,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton(' Add Me To Your Groups ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton(' Movies Hub Group ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
